404a7e9f8801aecc67eaa23167fe0b7a06d06e43
Raluca Cretu <ralucacr2000@yahoo.com>
2018-08-03 19:48:57 +0200

    Add InfoWindow for each marker

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -5,9 +5,12 @@ import React, {Component} from 'react'
 class MapContainer extends Component {
 
 	state = {
+		markers: [],
 		places: [],
 		error: null,
-		mapError: null
+		mapError: null,
+		infobox: new this.props.google.maps.InfoWindow(),
+		highlightedIcon: null
 	}
 
 	//Fetch data from Foursquare 
@@ -60,16 +63,17 @@ class MapContainer extends Component {
                 gestureHandling: 'cooperative'
             };
             this.map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-            //this.addMarkers()
+            this.addMarkers()
         }else {
             this.setState({mapError: "map error"})
         }
     }
 
     addMarkers = () => {
-    	const {markers} = this.this.state
+    	//const {markers} = this.this.state
     	const {google} = this.this.props
     	const bounds = new google.maps.LatLngBounds()
+    	let {infowindow}    = this.state
     	let i=0
 
 
@@ -85,12 +89,41 @@ class MapContainer extends Component {
 
 	        marker.addListener('click', () => {
 	        	const defaultIcon = marker.getIcon()//google map specific
+	        	const {highlightedIcon} = this.state
+
+	        	// if there is already a window open than close it
+                if (infowindow) {
+                    infowindow.close()
+                }
+                this.reset_markers()
+
+                //Add Content to Info window
+                infowindow = new google.maps.InfoWindow({ //google map specific
+                    content:    location.name+" in  "+location.location.city
+                });
+                infowindow.open( this.map, marker)//google map specific
+
+                //add animation and highlight the marker
+                marker.setAnimation(google.maps.Animation.BOUNCE)
+                marker.setIcon(highlightedIcon)
+
+                infowindow.addListener('closeclick', () => { //google map specific
+                    infowindow.marker = null
+                    marker.setIcon(defaultIcon)
+                    marker.setAnimation(null)
+                });
 	    	})
 
 	    	this.setState((state) => ({
                 markers: [...state.markers, marker]
+
             }))
+
+            bounds.extend(marker.position)
     	})
+
+    	console.log( "x "+this.state.markers)
+        this.map.fitBounds(bounds)
     }
 
 
