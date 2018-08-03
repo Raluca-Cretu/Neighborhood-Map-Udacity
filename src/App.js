757de7f7c979f548ebafc561951d99099d29f4eb
Raluca Cretu <ralucacr2000@yahoo.com>
2018-08-03 19:05:13 +0200

    Fetch data from Foursquare

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
           </svg>
+          <h1 className="heading" > Google Maps API for Schorndorf, Germany </h1>
         </a>
-        <h1 className="heading" > Google Maps API for Schorndorf, Germany </h1>
+        
         <MapContainer google={this.props.google}/>
       </div>
     );
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -5,14 +5,41 @@ import React, {Component} from 'react'
 class MapContainer extends Component {
 
 	state = {
-
+		places: [],
+		error: null,
+		mapError: null
 	}
 
 
 	componentDidMount() {
-		this.loadMap()
+		const url = 'https://api.foursquare.com/v2/venues/search?ll=48.8015423,9.52962&client_id=2ZMPRLINC4X0LQURACBFQUFWSWI55KVTBCBCG44RDEH3OJTW&client_secret=TS1ITUBV1K03MXUPFQDOFVZAEVF1GM45023JXVO5F1CT3SOI&v=20180801'
 
-	}
+        fetch(url)
+            .then(data => {
+                if(data.ok) {
+                  return data.json()
+                } else {
+                  throw new Error(data.statusText)
+                }
+            })
+
+            .then(data => {
+                this.setState({places: data.response.venues})
+                this.loadMap()
+                console.log(this.state.places)
+                //this.onclickLocation()
+            })
+
+            .catch(err => {
+                console.log(err)
+               this.setState({error: err.toString()})
+            })
+
+            //this.setState({highlightedIcon: this.changeMarkerColor('e0ff25')})
+            //this.setState({normalIcon: this.changeMarkerColor('ea4335')})
+
+    }
+
 
 
     loadMap() {
